4a58050523c19d00e86f14ff0cfb36d5edc45e23
wdcardenasb <38057700+wdcardenasb@users.noreply.github.com>
2018-05-08 19:22:57 -0500

    Propiedad Interfaz

---
+++ DataBinding/DataBinding/Persona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DataBinding
+{
+    using System.ComponentModel;
+    class Persona:INotifyPropertyChanged
+    {
+        #region Atributos
+        private string nombre;
+        private string pais;
+        #endregion
+
+        #region Propiedades
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+                OnPropertyChanged(Nombre);
+            }
+        }
+        public string Pais { get; set; }
+        #endregion
+
+        #region Implementación
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+
+    }
+
+
+}
